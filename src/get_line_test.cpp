76a437a054fc52213f8e74e45fbb64210ba6589b
Tishj <t_b@live.nl>
2021-02-02 18:59:52 +0100

    get_keyval added, returns a pair of strings

--- src/get_line_test.cpp
+++ src/get_line_test.cpp
@@ -6,7 +6,7 @@
 /*   By: tbruinem <tbruinem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/02 14:15:27 by tbruinem      #+#    #+#                 */
-/*   Updated: 2021/02/02 15:22:14 by tbruinem      ########   odam.nl         */
+/*   Updated: 2021/02/02 18:59:33 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 #include <unistd.h>
 #include <iostream>
 #include <limits>
+#include <utility>
 
+std::pair<std::string, std::string>	get_keyval(std::string raw, char delimiter = ':');
 std::vector<std::string>	get_lines(int fd, size_t max_lines = std::numeric_limits<size_t>::max());
 
 int main(int argc, char **argv)
@@ -31,7 +33,15 @@ int main(int argc, char **argv)
 		return (!!printf("Not a valid file\n"));
 	lines = get_lines(fd);
 	for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); it++)
+	{
+		size_t iskeyval = ((*it).find(':') == std::string::npos ? false : true);
+		if (iskeyval)
+		{
+			std::pair<std::string, std::string>	keyval = get_keyval(*it);
+			std::cout << "KEY: " << keyval.first << " | VAL: " << keyval.second << std::endl;
+		}
 		std::cout << *it << std::endl;
+	}
 	// while ((lines = get_lines(fd, 1)) != std::vector<std::string>())
 	// {
 	// 	std::cout << "PRINTED LINES[0] = " << lines[0] << std::endl;
