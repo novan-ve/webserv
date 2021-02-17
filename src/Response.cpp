27c4c4f53e78e4997fbecf9ee6199e5fb6d63fa9
Tishj <t_b@live.nl>
2021-02-17 11:14:30 +0100

    fucking forgot another st_mtim...

--- src/Response.cpp
+++ src/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: novan-ve <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/04 23:28:03 by novan-ve      #+#    #+#                 */
-/*   Updated: 2021/02/17 01:22:42 by tbruinem      ########   odam.nl         */
+/*   Updated: 2021/02/17 11:13:58 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -528,7 +528,7 @@ void	Response::setModified(void)
 
 	if (stat(this->path.c_str(), &result) == 0)
 	{
-		struct tm 	tm = ft::getTime(result.st_mtim.tv_sec);
+		struct tm 	tm = ft::getTime(result.st_mtime);
 		char		buf[64];
 
 		ft::memset(buf, '\0', 64);
